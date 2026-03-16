a4952ff76101f12bf6a1b63a64e827f637a154dc
shiken.ou <kaguraakann@gmail.com>
2026-03-16 19:39:15 +0900

    创建架构

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Integer, String, Text, DateTime, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime, timezone
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(Integer, primary_key= True)
+    username: Mapped[str] = mapped_column(String(10), unique= True, nullable= False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable= False)
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id: Mapped[int] = mapped_column(Integer, primary_key= True)
+    title: Mapped[str] = mapped_column(String(30), nullable= False)
+    content: Mapped[str] = mapped_column(Text, nullable= False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone= True), default= lambda :datetime.now(timezone.utc))
+
+
+engine = create_engine('sqlite:///instance/blog.db')
